cf9f8c8deec4fce6a22150bbe4f850bfdc2a2901
Pavelcaj <mr.derkach2000@mail.ru>
2023-08-10 06:41:33 +0300

    zd1

--- src/lesson4/zd1/zd1.java
+++ src/lesson4/zd1/zd1.java
@@ -1,6 +1,10 @@
 package lesson4.zd1;
 
 public class zd1 {
+    /**
+     Задача 1:
+     Сгенерировать 5 случайных чисел. Каждое возвести в квадрат и вывести в консоль
+     */
         public static void main(String[] args) {
             for(int i = 0; i < 5; i++){
                 int x = (int)((float)Math.random()*10);
